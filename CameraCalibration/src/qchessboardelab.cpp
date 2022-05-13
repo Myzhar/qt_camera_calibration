78258c7f897b84e30567c82c672c5fe36f46fe7d
aliakseis <aliaksei.sanko@gmail.com>
2022-05-13 10:58:46 +0300

    Update qchessboardelab.cpp

--- CameraCalibration/src/qchessboardelab.cpp
+++ CameraCalibration/src/qchessboardelab.cpp
@@ -4,6 +4,8 @@
 #include <utility>
 #include <vector>
 
+#include <QDebug>
+
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include "qcameracalibrate.h"
@@ -37,40 +39,44 @@ QChessboardElab::~QChessboardElab()
 
 void QChessboardElab::run()
 {
-    cv::Mat gray;
+    try {
+        cv::Mat gray;
 
-    // >>>>> Chessboard detection
-    cv::cvtColor( mFrame, gray,  cv::COLOR_BGR2GRAY );
-    vector<cv::Point2f> corners; //this will be filled by the detected corners
+        // >>>>> Chessboard detection
+        cv::cvtColor(mFrame, gray, cv::COLOR_BGR2GRAY);
+        vector<cv::Point2f> corners; //this will be filled by the detected corners
 
-    //CALIB_CB_FAST_CHECK saves a lot of time on images
-    //that do not contain any chessboard corners
-    bool found = findChessboardCorners(gray, mCbSize, corners,
-                                       cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE
-                                       + cv::CALIB_CB_FAST_CHECK);
+        //CALIB_CB_FAST_CHECK saves a lot of time on images
+        //that do not contain any chessboard corners
+        bool found = findChessboardCorners(gray, mCbSize, corners,
+            cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE
+            + cv::CALIB_CB_FAST_CHECK);
 
-    if(found)
-    {
-        emit cbFound();
-
-        cv::cornerSubPix( gray, corners, cv::Size(11, 11), cv::Size(-1, -1),
-                          cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 0.1));
-
-        cv::drawChessboardCorners( mFrame, mCbSize, cv::Mat(corners), found );
-
-        vector<cv::Point3f> obj;
-
-        int numSquares = mCbSize.width*mCbSize.height;
-        obj.reserve(numSquares);
-        for(int j=0;j<numSquares;j++)
+        if (found)
         {
-            obj.emplace_back((j/mCbSize.width)*mCbSizeMm, (j%mCbSize.width)*mCbSizeMm, 0.0F);
+            cv::cornerSubPix(gray, corners, cv::Size(11, 11), cv::Size(-1, -1),
+                cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 0.1));
+
+            emit cbFound();
+
+            cv::drawChessboardCorners(mFrame, mCbSize, cv::Mat(corners), found);
+
+            vector<cv::Point3f> obj;
+
+            int numSquares = mCbSize.width*mCbSize.height;
+            obj.reserve(numSquares);
+            for (int j = 0; j < numSquares; j++)
+            {
+                obj.emplace_back((j / mCbSize.width)*mCbSizeMm, (j%mCbSize.width)*mCbSizeMm, 0.0F);
+            }
+
+            mFisheyeUndist->addCorners(corners);
         }
-
-        mFisheyeUndist->addCorners( corners );
+        // <<<<< Chessboard detection
     }
-    // <<<<< Chessboard detection
-
+    catch (const std::exception& ex) {
+        qDebug() << __FUNCTION__ << ": exception: " << typeid(ex).name() << ": " << ex.what();
+    }
 
     emit newCbImage(mFrame);
 }
